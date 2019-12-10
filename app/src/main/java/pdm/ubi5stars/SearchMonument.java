226cc636989ae56ce8061287a9afdc1096ac0871
CallmeJoca <jocapissarra@protonmail.com>
2019-12-10 23:27:04 +0000

    Started Search Menu

---
+++ app/src/main/java/pdm/ubi5stars/SearchMonument.java
@@ -0,0 +1,34 @@
+package pdm.ubi5stars;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+// esta atividade representa o a pagina de pesquisa de monumentos por nome/tipo
+public class SearchMonument extends Activity {
+
+    private DatabaseHandler dbHelper;
+    private String monumento;
+    private int idMonumento;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_monument);
+
+        dbHelper = new DatabaseHandler(this);
+
+
+        
+
+    }
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        dbHelper.close();
+    }
+
+}
+
